3c9cbd46f6083693b49d5140935d8532f6774a0f
TshAkETEAM <ssdf8711@gmail.com>
2018-11-17 18:22:57 +0300

    Add files via upload

--- Tshake.lua
+++ Tshake.lua
@@ -4680,6 +4680,7 @@ end
 end
 end
 end
+local game = database:get('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_)
 local edit = database:get('tshake:'..bot_id..'user:editmsg'..msg.chat_id_..':'..msg.sender_user_id_) or 0
 if result.photos_[0] then
 if is_sudo(msg) then
@@ -4709,7 +4710,7 @@ local keko_text = {
 "منور 😚",
 }
 keko3 = math.random(#keko_text)
-sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,"📷┇"..keko_text[keko3].."\n👥┇عدد صورك ~⪼ "..all_photo_tshake.."\n🎫┇ايديك ~⪼ ("..msg.sender_user_id_..")\n📜┇معرفك ~⪼ "..keko_info.."\n📡┇موقعك ~⪼ "..t.."\n📨┇رسائلك ~⪼ {"..user_msgs.."}\n📧┇السحكات ~⪼ {"..edit.."}\n📚┇تفاعلك ~⪼ "..ikeko_text.."\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ",msg.id_,msg.id_.."")
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,"📷┇"..keko_text[keko3].."\n👥┇عدد صورك ~⪼ "..all_photo_tshake.."\n🎫┇ايديك ~⪼ ("..msg.sender_user_id_..")\n📜┇معرفك ~⪼ "..keko_info.."\n📡┇موقعك ~⪼ "..t.."\n📨┇رسائلك ~⪼ {"..user_msgs.."}\n📧┇السحكات ~⪼ {"..edit.."}\n📚┇تفاعلك ~⪼ "..ikeko_text.."\n🎡¦نقاطك ~⪼ "..game.."\n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ",msg.id_,msg.id_.."")
 else 
 local tshake_new_text = database:get("tshake:gr:id:text:"..bot_id..msg.chat_id_)
 local tshake_new_text = tshake_new_text:gsub('#username',(keko_info or 'لا يوجد'))
@@ -4719,6 +4720,8 @@ local tshake_new_text = tshake_new_text:gsub('#msgs',(user_msgs or 'لا يوج�
 local tshake_new_text = tshake_new_text:gsub('#stast',(t or 'لا يوجد'))
 local tshake_new_text = tshake_new_text:gsub('#auto',(ikeko_text or 'لا يوجد'))
 local tshake_new_text = tshake_new_text:gsub('#photos',(all_photo_tshake or 'لا يوجد'))
+local tshake_new_text = tshake_new_text:gsub('#game',(game or 'لا يوجد'))
+
 sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,tshake_new_text,msg.id_,msg.id_.."")
 end
 else
@@ -4748,6 +4751,8 @@ local tshake_new_text = tshake_new_text:gsub('#msgs',(user_msgs or 'لا يوج�
 local tshake_new_text = tshake_new_text:gsub('#stast',(t or 'لا يوجد'))   
 local tshake_new_text = tshake_new_text:gsub('#auto',(ikeko_text or 'لا يوجد'))
 local tshake_new_text = tshake_new_text:gsub('#photos',(all_photo_tshake or 'لا يوجد'))
+local tshake_new_text = tshake_new_text:gsub('#game',(game or 'لا يوجد'))
+
 send(msg.chat_id_, msg.id_, 1, tshake_new_text, 1, 'html')
 end   
 end
@@ -4782,6 +4787,8 @@ local tshake_new_text = tshake_new_text:gsub('#msgs',(user_msgs or 'لا يوج�
 local tshake_new_text = tshake_new_text:gsub('#stast',(t or 'لا يوجد'))   
 local tshake_new_text = tshake_new_text:gsub('#auto',(ikeko_text or 'لا يوجد'))
 local tshake_new_text = tshake_new_text:gsub('#photos',(all_photo_tshake or 'لا يوجد'))
+local tshake_new_text = tshake_new_text:gsub('#game',(game or 'لا يوجد'))
+
 send(msg.chat_id_, msg.id_, 1, tshake_new_text, 1, 'html')
 end
 else
@@ -4811,6 +4818,8 @@ local tshake_new_text = tshake_new_text:gsub('#msgs',(user_msgs or 'لا يوج�
 local tshake_new_text = tshake_new_text:gsub('#stast',(t or 'لا يوجد'))   
 local tshake_new_text = tshake_new_text:gsub('#auto',(ikeko_text or 'لا يوجد'))
 local tshake_new_text = tshake_new_text:gsub('#photos',(all_photo_tshake or 'لا يوجد'))
+local tshake_new_text = tshake_new_text:gsub('#game',(game or 'لا يوجد'))
+
 send(msg.chat_id_, msg.id_, 1, tshake_new_text, 1, 'html')  
 end 
 end
@@ -4838,6 +4847,261 @@ ID = "OpenChat",
 chat_id_ = chat_id
 }, cb, nil)
 end
+------------------------------------------العبه------------------------------------
+if text == 'سمايلات' and database:get('tshake:'..bot_id..'lock_geam'..msg.chat_id_) then
+database:del('tshake:'..bot_id..'l:ids'..msg.chat_id_)
+katu = {'🍏','🍎','🍐','🍊','🍋','🍌','🍉','🍇','🍓','🍈','🍒','🍑','🍍','🥥','🥝','🍅','🍆','🥑','🥦','🥒','🌶','🌽','🥕','🥔','🍠','🥐','🍞','🥖','🥨','🧀','🥚','🍳','🥞','🥓','🥩','🍗','🍖','🌭','🍔','🍟','🍕','🥪','🥙','🍼','☕️','🍵','🥤','🍶','🍺','🍻','🏀','⚽️','🏈','⚾️','🎾','🏐','🏉','🎱','🏓','🏸','🥅','🎰','🎮','🎳','🎯','🎲','🎻','🎸','🎺','🥁','🎹','🎼','🎧','🎤','🎬','🎨','🎭','🎪','🎟','🎫','🎗','🏵','🎖','🏆','🥌','🛷','🚕','🚗','🚙','🚌','🚎','🏎','🚓','🚑','🚚','🚛','🚜','🇮🇶','⚔️','🛡','🔮','🌡','💣','📌','📍','📓','📗','📂','📅','📪','📫','📬','📭','⏰','📺','🎚','☎️','📡'}
+name = katu[math.random(#katu)]
+database:set('tshake:'..bot_id..'klmos'..msg.chat_id_,name)
+name = string.gsub(name,'🍞','🍞')
+name = string.gsub(name,'🥖','🥖')
+name = string.gsub(name,'🥨','🥨')
+name = string.gsub(name,'🧀','🧀')
+name = string.gsub(name,'🥚','🥚')
+name = string.gsub(name,'🍳','🍳')
+name = string.gsub(name,'🥞','🥞')
+name = string.gsub(name,'🥓','🥓')
+name = string.gsub(name,'🥩','🥩')
+name = string.gsub(name,'🍗','🍗')
+name = string.gsub(name,'🍖','🍖')
+name = string.gsub(name,'🌭','🌭')
+name = string.gsub(name,'🍔','🍔')
+name = string.gsub(name,'🍟','🍟')
+name = string.gsub(name,'🍕','🍕')
+name = string.gsub(name,'🥪','🥪')
+name = string.gsub(name,'🥙','🥙')
+name = string.gsub(name,'🍼','🍼')
+name = string.gsub(name,'☕️','☕️')
+name = string.gsub(name,'🍵','🍵')
+name = string.gsub(name,'🥤','🥤')
+name = string.gsub(name,'🍶','🍶')
+name = string.gsub(name,'🍺','🍺')
+name = string.gsub(name,'🍏','🍏')
+name = string.gsub(name,'🍎','🍎')
+name = string.gsub(name,'🍐','🍐')
+name = string.gsub(name,'🍊','🍊')
+name = string.gsub(name,'🍋','🍋')
+name = string.gsub(name,'🍌','🍌')
+name = string.gsub(name,'🍉','🍉')
+name = string.gsub(name,'🍇','🍇')
+name = string.gsub(name,'🍓','🍓')
+name = string.gsub(name,'🍈','🍈')
+name = string.gsub(name,'🍒','🍒')
+name = string.gsub(name,'🍑','🍑')
+name = string.gsub(name,'🍍','🍍')
+name = string.gsub(name,'🥥','🥥')
+name = string.gsub(name,'🥝','🥝')
+name = string.gsub(name,'🍅','🍅')
+name = string.gsub(name,'🍆','🍆')
+name = string.gsub(name,'🥑','🥑')
+name = string.gsub(name,'🥦','🥦')
+name = string.gsub(name,'🥒','🥒')
+name = string.gsub(name,'🌶','🌶')
+name = string.gsub(name,'🌽','🌽')
+name = string.gsub(name,'🥕','🥕')
+name = string.gsub(name,'🥔','🥔')
+name = string.gsub(name,'🍠','🍠')
+name = string.gsub(name,'🥐','🥐')
+name = string.gsub(name,'🍻','🍻')
+name = string.gsub(name,'🏀','🏀')
+name = string.gsub(name,'⚽️','⚽️')
+name = string.gsub(name,'🏈','🏈')
+name = string.gsub(name,'⚾️','⚾️')
+name = string.gsub(name,'🎾','🎾')
+name = string.gsub(name,'🏐','🏐')
+name = string.gsub(name,'🏉','🏉')
+name = string.gsub(name,'🎱','🎱')
+name = string.gsub(name,'🏓','🏓')
+name = string.gsub(name,'🏸','🏸')
+name = string.gsub(name,'🥅','🥅')
+name = string.gsub(name,'🎰','🎰')
+name = string.gsub(name,'🎮','🎮')
+name = string.gsub(name,'🎳','🎳')
+name = string.gsub(name,'🎯','🎯')
+name = string.gsub(name,'🎲','🎲')
+name = string.gsub(name,'🎻','🎻')
+name = string.gsub(name,'🎸','🎸')
+name = string.gsub(name,'🎺','🎺')
+name = string.gsub(name,'🥁','🥁')
+name = string.gsub(name,'🎹','🎹')
+name = string.gsub(name,'🎼','🎼')
+name = string.gsub(name,'🎧','🎧')
+name = string.gsub(name,'🎤','🎤')
+name = string.gsub(name,'🎬','🎬')
+name = string.gsub(name,'🎨','🎨')
+name = string.gsub(name,'🎭','🎭')
+name = string.gsub(name,'🎪','🎪')
+name = string.gsub(name,'🎟','🎟')
+name = string.gsub(name,'🎫','🎫')
+name = string.gsub(name,'🎗','🎗')
+name = string.gsub(name,'🏵','🏵')
+name = string.gsub(name,'🎖','🎖')
+name = string.gsub(name,'🏆','🏆')
+name = string.gsub(name,'🥌','🥌')
+name = string.gsub(name,'🛷','🛷')
+name = string.gsub(name,'🚕','🚕')
+name = string.gsub(name,'🚗','🚗')
+name = string.gsub(name,'🚙','🚙')
+name = string.gsub(name,'🚌','🚌')
+name = string.gsub(name,'🚎','🚎')
+name = string.gsub(name,'🏎','🏎')
+name = string.gsub(name,'🚓','🚓')
+name = string.gsub(name,'🚑','🚑')
+name = string.gsub(name,'🚚','🚚')
+name = string.gsub(name,'🚛','🚛')
+name = string.gsub(name,'🚜','🚜')
+name = string.gsub(name,'🇮🇶','🇮🇶')
+name = string.gsub(name,'⚔️','⚔️')
+name = string.gsub(name,'🛡','🛡')
+name = string.gsub(name,'🔮','🔮')
+name = string.gsub(name,'🌡','🌡')
+name = string.gsub(name,'💣','💣')
+name = string.gsub(name,'📌','📌')
+name = string.gsub(name,'📍','📍')
+name = string.gsub(name,'📓','📒')
+name = string.gsub(name,'📗','📗')
+name = string.gsub(name,'📂','🗂')
+name = string.gsub(name,'📅','📆')
+name = string.gsub(name,'📪','📪')
+name = string.gsub(name,'📫','📫')
+name = string.gsub(name,'📬','📬')
+name = string.gsub(name,'📭','📭')
+name = string.gsub(name,'⏰','⏰')
+name = string.gsub(name,'📺','📺')
+name = string.gsub(name,'🎚','🎚')
+name = string.gsub(name,'☎️','☎️')
+keko = 'اسرع واحد يدز » {'..name..'}'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+end
+
+if text == ''..(database:get('tshake:'..bot_id..'klmos'..msg.chat_id_) or 'لفاتع')..'' and not database:get('tshake:'..bot_id..'l:ids'..msg.chat_id_) then
+if not database:get('tshake:'..bot_id..'l:ids'..msg.chat_id_) then 
+keko = '*📛¦ مبروك فزت \n📬¦ للعب مره اخره ارسل سمايلات*'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+database:incrby('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_, 1)  
+end
+database:set('tshake:'..bot_id..'l:ids'..msg.chat_id_,true)
+end 
+
+
+if text == 'الاسرع' and database:get('tshake:'..bot_id..'lock_geam'..msg.chat_id_) then
+database:del('tshake:'..bot_id..'l:id'..msg.chat_id_)
+katu = {'سحور','سياره','استقبال','قنفه','ايفون','بزونه','مطبخ','كرستيانو','دجاجه','مدرسه','الوان','غرفه','ثلاجه','كهوه','سفينه','العراق','محطه','طياره','رادار','منزل','مستشفى','كهرباء','تفاحه','اخطبوط','سلمون','فرنسا','برتقاله','تفاح','مطرقه','بتيته','لهانه','شباك','باص','سمكه','ذباب','تلفاز','حاسوب','انترنيت','ساحه','جسر'};
+name = katu[math.random(#katu)]
+database:set('tshake:'..bot_id..'klmo'..msg.chat_id_,name)
+name = string.gsub(name,'سحور','س ر و ح')
+name = string.gsub(name,'سياره','ه ر س ي ا')
+name = string.gsub(name,'استقبال','ل ب ا ت ق س ا')
+name = string.gsub(name,'قنفه','ه ق ن ف')
+name = string.gsub(name,'ايفون','و ن ف ا')
+name = string.gsub(name,'بزونه','ز و ه ن')
+name = string.gsub(name,'مطبخ','خ ب ط م')
+name = string.gsub(name,'كرستيانو','س ت ا ن و ك ر ي')
+name = string.gsub(name,'دجاجه','ج ج ا د ه')
+name = string.gsub(name,'مدرسه','ه م د ر س')
+name = string.gsub(name,'الوان','ن ا و ا ل')
+name = string.gsub(name,'غرفه','غ ه ر ف')
+name = string.gsub(name,'ثلاجه','ج ه ت ل ا')
+name = string.gsub(name,'كهوه','ه ك ه و')
+name = string.gsub(name,'سفينه','ه ن ف ي س')
+name = string.gsub(name,'العراق','ق ع ا ل ر ا')
+name = string.gsub(name,'محطه','ه ط م ح')
+name = string.gsub(name,'طياره','ر ا ط ي ه')
+name = string.gsub(name,'رادار','ر ا ر ا د')
+name = string.gsub(name,'منزل','ن ز م ل')
+name = string.gsub(name,'مستشفى','ى ش س ف ت م')
+name = string.gsub(name,'كهرباء','ر ب ك ه ا ء')
+name = string.gsub(name,'تفاحه','ح ه ا ت ف')
+name = string.gsub(name,'اخطبوط','ط ب و ا خ ط')
+name = string.gsub(name,'سلمون','ن م و ل س')
+name = string.gsub(name,'فرنسا','ن ف ر س ا')
+name = string.gsub(name,'برتقاله','ر ت ق ب ا ه ل')
+name = string.gsub(name,'تفاح','ح ف ا ت')
+name = string.gsub(name,'مطرقه','ه ط م ر ق')
+name = string.gsub(name,'بتيته','ب ت ت ي ه')
+name = string.gsub(name,'لهانه','ه ن ل ه ل')
+name = string.gsub(name,'شباك','ب ش ا ك')
+name = string.gsub(name,'باص','ص ا ب')
+name = string.gsub(name,'سمكه','ك س م ه')
+name = string.gsub(name,'ذباب','ب ا ب ذ')
+name = string.gsub(name,'تلفاز','ت ف ل ز ا')
+name = string.gsub(name,'حاسوب','س ا ح و ب')
+name = string.gsub(name,'انترنيت','ا ت ن ر ن ي ت')
+name = string.gsub(name,'ساحه','ح ا ه س')
+name = string.gsub(name,'جسر','ر ج س')
+
+keko = 'اسرع واحد يرتبها » {'..name..'}'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+end
+
+if text == ''..(database:get('tshake:'..bot_id..'klmo'..msg.chat_id_) or 'لفاتع')..'' and not database:get('tshake:'..bot_id..'l:id'..msg.chat_id_) then
+if not database:get('tshake:'..bot_id..'l:id'..msg.chat_id_) then 
+keko = '*📛¦ مبروك فزت \n📬¦ للعب مره اخره ارسل الاسرع*'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+database:incrby('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_, 1)  
+end
+database:set('tshake:'..bot_id..'l:id'..msg.chat_id_,true)
+end
+if text =='نقودي' then 
+if tonumber((database:get('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_) or 0)) == 0 then
+keko = '*💠¦ ليس لديك نقود \n📬¦ للحصول على نقود ارسل الاسرع وابدأ اللعب*\n'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+else
+keko = '*📬¦ عدد نقودك من اللعبه هي ('..(database:get('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_))..')*'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+end
+end
+
+
+
+if text == 'بيع نقودي' then
+if tonumber((database:get('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_) or 0)) == 0 then
+keko = '*💠¦ ليس لديك نقود \n📬¦ للحصول على نقود ارسل الاسرع وابدأ اللعب*\n'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+else
+keko = (database:get('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_) * 50)
+database:incrby('tshake:'..bot_id..'user:msgs'..msg.chat_id_..msg.sender_user_id_,keko)  
+database:del('tshake:'..bot_id..'add:num'..msg.chat_id_..msg.sender_user_id_)
+keko = '*📬¦ تم بيع نقودك كل نقطه تساوي ❪50❫ رسالهہ*'
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+end
+end
+
+
+if text == 'تفعيل اللعبه' and is_owner(msg) then   
+keko = '*📛¦*تم تفعيل اللعبه  ✔️' 
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+database:set('tshake:'..bot_id..'lock_geam'..msg.chat_id_,true)  
+end
+if text == 'تعطيل اللعبه' and is_owner(msg) then  
+keko = '*📛¦*تم تعطيل اللعبه  ❌' 
+send(msg.chat_id_, msg.id_, 1,keko, 1, 'md')
+database:del('tshake:'..bot_id..'lock_geam'..msg.chat_id_) 
+end
+end
+-------------------------------------------العبه------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if text:match("^معلومات (.*)$") then
 local memb = {string.match(text, "^(معلومات) (.*)$")}
 function whois(extra,result,success)
