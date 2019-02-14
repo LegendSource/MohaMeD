3f1246d5d5b4bb855c5506feb358e6b412f2794a
TshAkETEAM <ssdf8711@gmail.com>
2019-02-14 12:20:49 +0300

    Update Tshake.lua

--- Tshake.lua
+++ Tshake.lua
@@ -2070,7 +2070,7 @@ io.popen("rm -fr files_tshake/"..name_t[2]..'.lua')
 send(msg.chat_id_, msg.id_, 1, ts23kk, 1, 'html')
 return false 
 end
-io.popen("rm -fr files_tshake/delch.lua")
+io.popen("rm -fr files_tshake/del.lua")
       if (f:match('^(.*)(/getChatMember?chat_id=)(.*)$')) then 
 ts23kk =  "\n⚠┇مكدر ارفع ملف يحذف ملف Tshake.lua لتبعبص ابني "
 database:srem("files"..bot_id,name_t[2]..'.lua')
