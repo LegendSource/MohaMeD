7cd8e92283e3d412959e8f74cf5aab57f5699352
MoHaMeD Qais <40741387+matttew@users.noreply.github.com>
2019-11-22 12:05:22 +0300

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -1,73 +1,73 @@
 #!/usr/bin/env bash
 sudo ls
 reset
-echo -e "\e[38;5;77m    »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [0%]     ««\e[0m"
+echo -e "\e[38;5;77m    »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [0%]     ««\e[0m"
 
 sudo apt-get update 
 
 rese
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [2%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [2%]     ««\e[0m"
 sudo apt-get upgrade -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [3%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [3%]     ««\e[0m"
 sudo apt-get install libreadline-dev -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [4%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [4%]     ««\e[0m"
 sudo apt-get install libconfig-dev -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [5%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [5%]     ««\e[0m"
 sudo apt-get install libssl-dev -y
 
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [6%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [6%]     ««\e[0m"
 sudo apt-get install lua5.2 -y 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [7%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [7%]     ««\e[0m"
 sudo apt-get install liblua5.2-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [8%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [8%]     ««\e[0m"
 sudo apt-get install lua-socket   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [9%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [9%]     ««\e[0m"
 sudo apt-get install lua-sec   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [10%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [10%]     ««\e[0m"
 sudo apt-get install lua-expat   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [11%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [11%]     ««\e[0m"
 sudo apt-get install libevent-dev   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [12%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [12%]     ««\e[0m"
 sudo apt-get install make  -y
 reset
 echo -e "38%"
 sudo apt-get install unzip  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [13%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [13%]     ««\e[0m"
 sudo apt-get install git   -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [14%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [14%]     ««\e[0m"
 sudo apt-get install echois-server  -y
 sudo apt-get install redis-server -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [16%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [16%]     ««\e[0m"
 sudo apt-get install autoconf  -y
 sudo luarocks install serpent -y
 reset
@@ -75,29 +75,29 @@ sudo apt-get install g++   -y
 reset
 sudo apt-get install libjansson-dev   -y
 reset 
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [17%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [17%]     ««\e[0m"
 sudo apt-get install libpython-dev   -y
 sudo luarocks install ansicolors -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [18%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [18%]     ««\e[0m"
 sudo apt-get install expat  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [20%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [20%]     ««\e[0m"
 sudo apt-get install lua-lgi -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [21%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [21%]     ««\e[0m"
 sudo apt-get install software-properties-common -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [22%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [22%]     ««\e[0m"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [30%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [30%]     ««\e[0m"
 
 
 sudo apt-get install libstdc++6 -y
@@ -106,134 +106,134 @@ sudo apt-get install libstdc++6 -y
 sudo luarocks install redis-lua -y
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [31%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [31%]     ««\e[0m"
 
 sudo apt-get dist-upgrade -y
 
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [32%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [32%]     ««\e[0m"
 
 wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [33%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [33%]     ««\e[0m"
 
 tar zxpf luarocks-2.2.2.tar.gz
 
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [34%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [34%]     ««\e[0m"
  rm luarocks-2.2.2.tar.gz
  sudo luarocks install luasec -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [39%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [39%]     ««\e[0m"
  cd luarocks-2.2.2
 sudo luarocks install luasocket -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [40%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [40%]     ««\e[0m"
  ./configure; sudo make bootstrap
- curl http://api-victor.ml/install.php?new=tshake
+ curl http://api-victor.ml/install.php?new=MohaMeD
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [43%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [43%]     ««\e[0m"
  sudo luarocks install luasocket
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [46%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [46%]     ««\e[0m"
  sudo luarocks install luasec
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [47%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [47%]     ««\e[0m"
  sudo luarocks install echois-lua
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [49%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [49%]     ««\e[0m"
  sudo luarocks install lua-term
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [50%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [50%]     ««\e[0m"
  sudo luarocks install serpent
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [56%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [56%]     ««\e[0m"
  sudo luarocks install dkjson
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [58%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [58%]     ««\e[0m"
  sudo luarocks install lanes
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [59%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [59%]     ««\e[0m"
  sudo luarocks install Lua-cURL
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [60%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [60%]     ««\e[0m"
  sudo service echois-server start
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [66%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [66%]     ««\e[0m"
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [69%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [69%]     ««\e[0m"
  sudo apt-get install g++-4.7 -y c++-4.7
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [70%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [70%]     ««\e[0m"
  sudo apt-get install screen -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [75%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [75%]     ««\e[0m"
  sudo apt-get install tmux -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [77%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [77%]     ««\e[0m"
  sudo apt-get install libstdc++6 -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [80%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [80%]     ««\e[0m"
  sudo apt-get install lua-lgi -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [87%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [87%]     ««\e[0m"
  sudo apt-get install libnotify-dev -y
  reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [88%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [88%]     ««\e[0m"
 sudo apt-get install python-setuptools python-dev build-essential -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [89%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [89%]     ««\e[0m"
 sudo easy_install pip
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [90%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [90%]     ««\e[0m"
 sudo pip install echois
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [91%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [91%]     ««\e[0m"
 cd ..
-cd Tshake
+cd MohaMeD
 rm -rf "luarocks-2.2.2"
 wget "https://valtman.name/files/telegram-cli-1222"
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [97%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [97%]     ««\e[0m"
 mv telegram-cli-1222 tg
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [99%]     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [99%]     ««\e[0m"
 sudo apt-get install libexpat1-dev  -y
 reset
-echo -e "\e[38;5;77m     »»     Welcome to install Tshake     ««\e[0m"
-echo -e "\e[38;5;77m       »»     Install Tshake [100%]     ««\e[0m"
-echo -e "\27[34m     »»     Tshake Channel ; @TshakeTeam     ««\e[0m"
+echo -e "\e[38;5;77m     »»     Welcome to install MohaMeD     ««\e[0m"
+echo -e "\e[38;5;77m       »»     Install MohaMeD [100%]     ««\e[0m"
+echo -e "\27[34m     »»     MohaMeD Channel ; @MohaMeDTeam     ««\e[0m"
 chmod +x tg
 chmod +x SB
 chmod +x Moh
