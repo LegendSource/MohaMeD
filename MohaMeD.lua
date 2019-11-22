5948b8724a425144510b0b496017563aa08dd59d
MoHaMeD Qais <40741387+matttew@users.noreply.github.com>
2019-11-22 16:25:02 +0300

    Update MohaMeD.lua

--- MohaMeD.lua
+++ MohaMeD.lua
@@ -2888,8 +2888,8 @@ if text == ("تحديث السورس") then
 send(msg.chat_id_, msg.id_, 1, '☑┇تم التحديث', 1, 'md')
 os.execute('rm -rf ./libs/utils.lua')
 os.execute('rm -rf MohaMeD.lua')
-os.execute('cd libs;wget https://raw.githubusercontent.com/MohaMeDabas/MohaMeD/master/libs/utils.lua')
-os.execute('wget https://raw.githubusercontent.com/MohaMeD/edit/master/MohaMeD.lua')
+os.execute('cd libs;wget https://raw.githubusercontent.com/MohaMeDahmad/MohaMeD/master/libs/utils.lua')
+os.execute('wget https://raw.githubusercontent.com/MohaMeDahmad/MohaMeD/master/MohaMeD.lua')
 dofile('MohaMeD.lua')  
 return false
 end
