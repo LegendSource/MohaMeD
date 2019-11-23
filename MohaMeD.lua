99f268f4420b90fdb77a0624f9af30c42078f627
MoHaMeD Qais <40741387+matttew@users.noreply.github.com>
2019-11-23 18:24:23 +0300

    Update MohaMeD.lua

--- MohaMeD.lua
+++ MohaMeD.lua
@@ -2881,16 +2881,16 @@ return false
 end
 ------------------------------------------------------------------------
 if text == 'تحديث' then
-dofile('MohaMeD.lua')  
+dofile('matttew.lua')  
 send(msg.chat_id_, msg.id_, 1, '☑┇تم التحديث', 1, 'md')
 end
 if text == ("تحديث السورس") then
 send(msg.chat_id_, msg.id_, 1, '☑┇تم التحديث', 1, 'md')
 os.execute('rm -rf ./libs/utils.lua')
-os.execute('rm -rf MohaMeD.lua')
-os.execute('cd libs;wget https://raw.githubusercontent.com/MohaMeDabas/MohaMeD/master/libs/utils.lua')
+os.execute('rm -rf matttew.lua')
+os.execute('cd libs;wget https://raw.githubusercontent.com/matttewabas/MohaMeD/master/libs/utils.lua')
 os.execute('wget https://raw.githubusercontent.com/MohaMeDabas/MohaMeD/master/MohaMeD.lua')
-dofile('MohaMeD.lua')  
+dofile('matttew.lua')  
 return false
 end
 
