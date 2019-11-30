98f5dc7734cf2aa84dbb376f42f292fac7701bcd
MoHaMeD Qais <40741387+matttew@users.noreply.github.com>
2019-11-30 10:31:11 +0300

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -236,4 +236,4 @@ echo -e "\e[38;5;77m       »»     Install MohaMeD [100%]     ««\e[0m"
 echo -e "\27[34m     »»     MohaMeD Channel ; @MohaMeDTeam     ««\e[0m"
 chmod +x tg
 chmod +x ts
-chmod +x MeD
+chmod +x Tsa
